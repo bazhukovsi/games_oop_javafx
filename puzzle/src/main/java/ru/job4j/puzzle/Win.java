1042afac766f44892bd2d5026d20085c076d3796
Сергей <bazhukovsi@yandex.ru>
2021-12-21 19:07:58 +0300

    2. Сокобан. Тестовое задание. [#75283 #242541]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -4,7 +4,7 @@ public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = false;
         for (int i = 0; i < board.length; i++) {
-            if (horizontalLine(board, i) || verticalLine(board, i)) {
+            if (board[i][i] == 1 && (horizontalLine(board, i) || verticalLine(board, i)))  {
                 rsl = true;
                 break;
             }
